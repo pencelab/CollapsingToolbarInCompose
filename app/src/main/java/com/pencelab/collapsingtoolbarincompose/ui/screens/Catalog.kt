833184f60be487f8a3cd844a3a5aace251d8f16b
Glenn <glenn906@gmail.com>
2022-05-29 19:12:46 -0600

    :sparkles: Implement a collapsing toolbar with the following scroll flags: 'scroll', 'enterAlways', 'enterAlwaysCollapsed' and 'exitUntilCollapsed'.

--- app/src/main/java/com/pencelab/collapsingtoolbarincompose/ui/screens/Catalog.kt
+++ app/src/main/java/com/pencelab/collapsingtoolbarincompose/ui/screens/Catalog.kt
@@ -1,9 +1,13 @@
 package com.pencelab.collapsingtoolbarincompose.ui.screens
 
+import androidx.compose.animation.core.FloatExponentialDecaySpec
+import androidx.compose.animation.core.animateDecay
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
@@ -11,9 +15,11 @@ import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
 import androidx.compose.ui.input.nestedscroll.NestedScrollSource
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.Velocity
 import androidx.compose.ui.unit.dp
 import com.pencelab.collapsingtoolbarincompose.AnimalListPreviewParameterProvider
 import com.pencelab.collapsingtoolbarincompose.data.model.Animal
@@ -24,6 +30,8 @@ import com.pencelab.collapsingtoolbarincompose.ui.management.states.toolbar.Fixe
 import com.pencelab.collapsingtoolbarincompose.ui.management.states.toolbar.ToolbarState
 import com.pencelab.collapsingtoolbarincompose.ui.management.states.toolbar.scrollflags.*
 import com.pencelab.collapsingtoolbarincompose.ui.theme.CollapsingToolbarInComposeTheme
+import kotlinx.coroutines.cancelChildren
+import kotlinx.coroutines.launch
 
 private val MinToolbarHeight = 96.dp
 private val MaxToolbarHeight = 176.dp
@@ -66,12 +74,32 @@ fun Catalog(
     val toolbarState = rememberToolbarState(toolbarHeightRange)
     val listState = rememberLazyListState()
 
+    val scope = rememberCoroutineScope()
+
     val nestedScrollConnection = remember {
         object : NestedScrollConnection {
             override fun onPreScroll(available: Offset, source: NestedScrollSource): Offset {
                 toolbarState.scrollTopLimitReached = listState.firstVisibleItemIndex == 0 && listState.firstVisibleItemScrollOffset == 0
                 toolbarState.scrollOffset = toolbarState.scrollOffset - available.y
                 return Offset(0f, toolbarState.consumed)
+            }
+
+            override suspend fun onPostFling(consumed: Velocity, available: Velocity): Velocity {
+                if (available.y > 0) {
+                    scope.launch {
+                        animateDecay(
+                            initialValue = toolbarState.height + toolbarState.offset,
+                            initialVelocity = available.y,
+                            animationSpec = FloatExponentialDecaySpec()
+                        ) { value, velocity ->
+                            toolbarState.scrollTopLimitReached = listState.firstVisibleItemIndex == 0 && listState.firstVisibleItemScrollOffset == 0
+                            toolbarState.scrollOffset = toolbarState.scrollOffset - (value - (toolbarState.height + toolbarState.offset))
+                            if (toolbarState.scrollOffset == 0f) scope.coroutineContext.cancelChildren()
+                        }
+                    }
+                }
+
+                return super.onPostFling(consumed, available)
             }
         }
     }
@@ -82,7 +110,12 @@ fun Catalog(
             columns = columns,
             modifier = Modifier
                 .fillMaxSize()
-                .graphicsLayer { translationY = toolbarState.height + toolbarState.offset },
+                .graphicsLayer { translationY = toolbarState.height + toolbarState.offset }
+                .pointerInput(Unit) {
+                    detectTapGestures(
+                        onPress = { scope.coroutineContext.cancelChildren() }
+                    )
+                },
             listState = listState,
             contentPadding = PaddingValues(bottom = if (toolbarState is FixedScrollFlagState) MinToolbarHeight else 0.dp)
         )
